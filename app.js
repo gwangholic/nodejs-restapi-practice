41f80703ee059edf99f8b9f46a840bc22c005853
gwangholic <gafordev@gmail.com>
2020-08-31 14:12:00 +0900

    url routing, static

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
 app.listen(3000, function () {
-  console.log();
+  console.log("start, express server on port 3000");
+});
+app.use(express.static("public"));
+
+// url routing
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/main.html");
 });
